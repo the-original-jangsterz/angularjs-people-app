8b7432ff354902e307e93557d46738851cc4b4e6
Peter Jang <peter.x.jang@gmail.com>
2016-08-16 19:44:33 -0500

    Add people api show and create actions

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::PeopleController < ApplicationController
     @people = Person.all
     render 'index.json.jbuilder'
   end
+
+  def show
+    @person = Person.find_by(id: params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @person = Person.new(
+      name: params[:name],
+      bio: params[:bio]
+    )
+    @person.save
+    render 'show.json.jbuilder'
+  end
 end
